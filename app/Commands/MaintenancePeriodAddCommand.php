9aecbc55bca2c77c48117276e782bf9b9f17696d
Owen Voke <development@voke.dev>
2020-12-01 13:55:43 +0000

    feat: add command to add maintenance periods

---
+++ app/Commands/MaintenancePeriodAddCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+use OhDear\PhpSdk\OhDear;
+
+class MaintenancePeriodAddCommand extends Command
+{
+    /** @var string */
+    protected $signature = 'maintenance-period:add
+                            {site-id : The id of the site that you want to create a maintenance period for}
+                            {start-date : The start date that you want to create a maintenance period for}
+                            {end-date : The end date that you want to create a maintenance period for}';
+
+    /** @var string */
+    protected $description = 'Add a new maintenance period for a site';
+
+    public function handle(OhDear $ohDear): void
+    {
+        if (! $startDate = $this->argument('start-date')) {
+            $this->warn('A valid start date must be provided');
+
+            return;
+        }
+
+        if (! $endDate = $this->argument('end-date')) {
+            $this->warn('A valid end date must be provided');
+
+            return;
+        }
+
+        $maintenancePeriod = $ohDear->createSiteMaintenance($this->argument('id'), $startDate, $endDate);
+
+        $this->table(['ID', 'Site ID', 'Starts At', 'Ends At'], [
+            $maintenancePeriod->id,
+            $maintenancePeriod->siteId,
+            $maintenancePeriod->startsAt,
+            $maintenancePeriod->endsAt,
+        ]);
+    }
+}
