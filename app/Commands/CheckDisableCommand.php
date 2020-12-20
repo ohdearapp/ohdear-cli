55d71a2e25144960e889fa7e0410b3ec40f14787
Owen Voke <development@voke.dev>
2020-12-20 17:26:13 +0000

    feat: add command to disable a check

---
+++ app/Commands/CheckDisableCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Commands;
+
+use App\Commands\Concerns\EnsureHasToken;
+use LaravelZero\Framework\Commands\Command;
+use OhDear\PhpSdk\OhDear;
+
+class CheckDisableCommand extends Command
+{
+    use EnsureHasToken;
+
+    /** @var string */
+    protected $signature = 'check:disable {id : The id of the check to disable}';
+
+    /** @var string */
+    protected $description = 'Disable a specific check';
+
+    public function handle(OhDear $ohDear)
+    {
+        if (! $this->ensureHasToken()) {
+            return 1;
+        }
+
+        $ohDear->disableCheck($this->argument('id'));
+
+        $this->info("Disabled the check with id {$this->argument('id')}");
+    }
+}
