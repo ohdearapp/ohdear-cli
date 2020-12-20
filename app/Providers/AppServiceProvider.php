d8f52b0041ebb655c3fb0b386b19feb96fa146cd
Owen Voke <development@voke.dev>
2020-12-20 14:32:32 +0000

    feat: remove runtime exception when key is not set

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,18 +4,13 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use OhDear\PhpSdk\OhDear;
-use RuntimeException;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         $this->app->singleton(OhDear::class, function () {
-            if (! $apiToken = config('settings.ohdear_api_token')) {
-                throw new RuntimeException('Oh Dear API key not set');
-            }
-
-            return new OhDear($apiToken);
+            return new OhDear(config('settings.ohdear_api_token'));
         });
     }
 
