d4c4014aac08dd56d8cf92843db307a69ecc7482
Owen Voke <development@voke.dev>
2020-12-20 17:26:02 +0000

    feat: add command to enable a check

---
+++ app/Commands/CheckEnableCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Commands;
+
+use App\Commands\Concerns\EnsureHasToken;
+use LaravelZero\Framework\Commands\Command;
+use OhDear\PhpSdk\OhDear;
+
+class CheckEnableCommand extends Command
+{
+    use EnsureHasToken;
+
+    /** @var string */
+    protected $signature = 'check:enable {id : The id of the check to enable}';
+
+    /** @var string */
+    protected $description = 'Enable a specific check';
+
+    public function handle(OhDear $ohDear)
+    {
+        if (!$this->ensureHasToken()) {
+            return 1;
+        }
+
+        $ohDear->enableCheck($this->argument('id'));
+
+        $this->info("Enabled the check with id {$this->argument('id')}");
+    }
+}
