9bf31a392cac9bfc5935505385afe9f03fdc2baa
Owen Voke <development@voke.dev>
2021-01-14 11:02:30 +0000

    style: apply fixes from Style CI

--- app/Commands/CronCheckShowCommand.php
+++ app/Commands/CronCheckShowCommand.php
@@ -34,6 +34,7 @@ class CronCheckShowCommand extends Command
         $this->output->listing(
             collect($cronChecks)->map(static function (CronCheck $cronCheck) {
                 $schedule = $cronCheck->cronExpression ?? "every {$frequencyInMinutes} minutes";
+
                 return "{$cronCheck->name} (schedule: {$schedule}, grace time: {$cronCheck->graceTimeInMinutes} minutes) (ping url: {$cronCheck->pingUrl})";
             })->toArray()
         );
