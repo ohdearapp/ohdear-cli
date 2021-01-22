c2d0f7de0e7e3eef76635498196bb6b82e78f710
Owen Voke <development@voke.dev>
2021-01-22 12:44:10 +0000

    feat: add command to delete cron check

---
+++ app/Commands/CronCheckDeleteCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Commands;
+
+use App\Commands\Concerns\EnsureHasToken;
+use LaravelZero\Framework\Commands\Command;
+use OhDear\PhpSdk\OhDear;
+
+class CronCheckDeleteCommand extends Command
+{
+    use EnsureHasToken;
+
+    /** @var string */
+    protected $signature = 'cron-check:delete
+                            {id : The id of the cron check that you want to delete}';
+
+    /** @var string */
+    protected $description = 'Delete a cron check';
+
+    public function handle(OhDear $ohDear)
+    {
+        if (! $this->ensureHasToken()) {
+            return 1;
+        }
+
+        $ohDear->deleteCronCheck($this->argument('id'));
+
+        $this->info("Removed the cron check with id {$this->argument('id')}");
+    }
+}
