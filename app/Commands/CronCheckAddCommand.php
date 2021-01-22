2031bfb85d98167c5f2af2b85f95fab4eafc090e
Owen Voke <development@voke.dev>
2021-01-22 13:02:00 +0000

    feat: add command to create cron check

---
+++ app/Commands/CronCheckAddCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Commands;
+
+use App\Commands\Concerns\EnsureHasToken;
+use LaravelZero\Framework\Commands\Command;
+use OhDear\PhpSdk\OhDear;
+
+class CronCheckAddCommand extends Command
+{
+    use EnsureHasToken;
+
+    /** @var string */
+    protected $signature = 'cron-check:add
+                            {site-id : The id of the site that you want to create a cron check for}
+                            {name : The name of the cron check}
+                            {frequency-or-expression : The frequency of the cron check in minutes, or cron expression}
+                            {--grace-time=5 : The grace time in minutes}
+                            {--description= : The description for the cron check}
+                            {--timezone=UTC : The timezone of your server}';
+
+    /** @var string */
+    protected $description = 'Add a new cron check for a site';
+
+    public function handle(OhDear $ohDear)
+    {
+        if (! $this->ensureHasToken()) {
+            return 1;
+        }
+
+        if (! $name = $this->argument('name')) {
+            $this->warn('A valid name must be provided');
+
+            return;
+        }
+
+        $cronCheck = is_numeric($this->argument('frequency-or-expression')) ?
+            $ohDear->createSimpleCronCheck(
+                $this->argument('site-id'),
+                $name,
+                $this->argument('frequency-or-expression'),
+                (int) $this->option('grace-time'),
+                $this->option('description'),
+            ) :
+            $ohDear->createCronCheck(
+                $this->argument('site-id'),
+                $name,
+                $this->argument('frequency-or-expression'),
+                (int) $this->option('grace-time'),
+                $this->option('description'),
+                $this->option('timezone')
+            );
+
+        $schedule = $cronCheck->cronExpression ?? "every {$cronCheck->frequencyInMinutes} minutes";
+
+        return "{$cronCheck->name} (schedule: {$schedule}, grace time: {$cronCheck->graceTimeInMinutes} minutes) (ping url: {$cronCheck->pingUrl})";
+    }
+}
