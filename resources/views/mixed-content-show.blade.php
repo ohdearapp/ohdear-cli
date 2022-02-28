4078ecb0ab16a4b869f228fff90297a3ab7a1885
Owen Voke <development@voke.dev>
2022-02-28 20:45:31 +0000

    feat: use Termwind for `mixed-content:show`

---
+++ resources/views/mixed-content-show.blade.php
@@ -0,0 +1,23 @@
+<div class="ml-2 my-1">
+    @forelse($mixedContentList as $foundOnUrl => $mixedContents)
+        <div>
+            <div class="underline mt-1">{{ $foundOnUrl }}</div>
+
+            <ul>
+                @forelse ($mixedContents as $mixedContent)
+                    <li>
+                        <span class="font-bold text-gray">{{ $mixedContent->elementName }}</span> ({{ $mixedContent->mixedContentUrl }})
+                    </li>
+                @empty
+                    <li class="list-none">
+                        <span>No mixed content was found for the specified site.</span>
+                    </li>
+                @endforelse
+            </ul>
+        </div>
+    @empty
+        <div>
+            <span>No mixed content was found for the specified site.</span>
+        </div>
+    @endforelse
+</div>
