76428a96f46fe099a8348804a88dbdca0ac45c2e
Owen Voke <development@voke.dev>
2022-02-28 16:54:37 +0000

    feat: use Termwind for `dns-history:show`

---
+++ app/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (! function_exists('format_dns_type')) {
+    function format_dns_type(array $dnsRecord): string
+    {
+        return match ($dnsRecord['type']) {
+            'A' => (string) ($dnsRecord['ip']),
+            'AAAA' => (string) ($dnsRecord['ipv6']),
+            'MX' => "{$dnsRecord['target']} / PRI {$dnsRecord['pri']}",
+            'NS' => (string) ($dnsRecord['target']),
+            'SOA' => "{$dnsRecord['mname']} / SN {$dnsRecord['serial']}",
+            'TXT' => (string) ($dnsRecord['txt']),
+            default => (string) ($dnsRecord['type'])
+        };
+    }
+}
