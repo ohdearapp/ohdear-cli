45a54b72292a3a4ba98766fe8e62586b4f09b974
Owen Voke <development@voke.dev>
2022-03-01 19:34:06 +0000

    feat: use Termwind for `broken-link:show`

---
+++ resources/views/broken-link-show.blade.php
@@ -0,0 +1,23 @@
+<div class="ml-2 my-1">
+    @forelse($brokenLinkList as $foundOnUrl => $brokenLinks)
+        <div>
+            <div class="underline mt-1">{{ $foundOnUrl }}</div>
+
+            <ul>
+                @forelse ($brokenLinks as $brokenLink)
+                    <li>
+                        <span class="font-bold text-gray">{{ $brokenLink->statusCode }}</span> ({{ $brokenLink->crawledUrl }})
+                    </li>
+                @empty
+                    <li class="list-none">
+                        <span>No broken links were found for the specified site.</span>
+                    </li>
+                @endforelse
+            </ul>
+        </div>
+    @empty
+        <div>
+            <span>No broken links were found for the specified site.</span>
+        </div>
+    @endforelse
+</div>
