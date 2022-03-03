6c5caf92c12d19d8a0ff033cfb8d474e3b844c22
Owen Voke <development@voke.dev>
2022-03-03 09:32:23 +0000

    chore: sync commands config with core

--- config/commands.php
+++ config/commands.php
@@ -55,11 +55,13 @@ return [
     */
 
     'hidden' => [
-        Symfony\Component\Console\Command\HelpCommand::class,
         NunoMaduro\LaravelConsoleSummary\SummaryCommand::class,
+        Symfony\Component\Console\Command\DumpCompletionCommand::class,
+        Symfony\Component\Console\Command\HelpCommand::class,
         Illuminate\Console\Scheduling\ScheduleRunCommand::class,
-        Illuminate\Foundation\Console\VendorPublishCommand::class,
+        Illuminate\Console\Scheduling\ScheduleListCommand::class,
         Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
+        LaravelZero\Framework\Commands\StubPublishCommand::class,
     ],
 
     /*
